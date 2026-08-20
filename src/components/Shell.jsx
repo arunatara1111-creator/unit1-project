721a395710b14fa23f772f15c130358f260e782b
Aruna <arunatara1111@gmail.com>
2026-08-20 10:28:58 -0500

    childview and shell added

---
+++ src/components/Shell.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/child.css";
+
+export default function Shell({ children }) {
+  return (
+    <div className="child-app">
+      {children}
+    </div>
+  );
+}
